f351abdd686180370143182e86e28b88c7adb3d7
Ada Xiang <adaxiang@Adas-MacBook-Pro.local>
2022-05-11 09:44:00 +0800

    added config

--- config.py
+++ config.py
@@ -5,4 +5,9 @@ TEST_MODE = True
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 if TEST_MODE:
-    FIEL_DIR = os.path.join(BASE_DIR, 'test', 'files')
+    pass
+    # FIEL_DIR = os.path.join(BASE_DIR, 'test', 'files')
+
+FIEL_DIR = os.path.join(BASE_DIR, 'files')
+KOBO_DB_DIR = os.path.join(BASE_DIR, 'files', 'KoboReader.sqlite')
+BASH_DIR = os.path.join(BASE_DIR, 'senders', 'append.sh')
