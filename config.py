afcd8862f5666b0257662c76b3a5f614e0da8d43
Ada Xiang <adaxiang@Adas-MacBook-Pro.local>
2022-05-24 11:10:03 +0800

    edited DIR setting; added file extenstion setting and CORS setting

--- config.py
+++ config.py
@@ -2,12 +2,18 @@ import os
 
 TEST_MODE = True
 
+VALID_FILE_EXTENSION = ['.txt', '.sqlite']
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 if TEST_MODE:
     pass
     # FIEL_DIR = os.path.join(BASE_DIR, 'test', 'files')
 
-FIEL_DIR = os.path.join(BASE_DIR, 'files')
+FILE_DIR = os.path.join(BASE_DIR, 'files')
+KOBO_FILE_DIR = os.path.join(FILE_DIR, 'kobo')
+KINDLE_FILE_DIR = os.path.join(FILE_DIR, 'kindle')
 KOBO_DB_DIR = os.path.join(BASE_DIR, 'files', 'KoboReader.sqlite')
 BASH_DIR = os.path.join(BASE_DIR, 'senders', 'append.sh')
+
+API_ORIGINS = ['http://localhost:4200']
